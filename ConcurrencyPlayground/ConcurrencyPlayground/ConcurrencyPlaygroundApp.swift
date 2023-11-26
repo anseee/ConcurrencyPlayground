f07a79cd84f5df34864f144c2371c6775bc8388c
anseee <parksungwon1113@gmail.com>
2023-11-26 14:48:00 +0900

    Initial commit

---
+++ ConcurrencyPlayground/ConcurrencyPlayground/ConcurrencyPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyPlaygroundApp.swift
+//  ConcurrencyPlayground
+//
+//  Created by 박성원 on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
