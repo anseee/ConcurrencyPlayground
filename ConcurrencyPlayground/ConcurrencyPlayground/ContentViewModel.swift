cad8fac330c57b4153827336f17987755390b482
anseee <parksungwon1113@gmail.com>
2023-11-26 16:45:29 +0900

    [+][*] BankAccount Unit Test 추가.

---
+++ ConcurrencyPlayground/ConcurrencyPlayground/ContentViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentViewModel.swift
+//  ConcurrencyPlayground
+//
+//  Created by 박성원 on 11/26/23.
+//
+
+import Foundation
--- ConcurrencyPlayground/ConcurrencyPlaygroundTests/ConcurrencyPlaygroundTests.swift
+++ ConcurrencyPlayground/ConcurrencyPlaygroundTests/ConcurrencyPlaygroundTests.swift
@@ -9,28 +9,53 @@ import XCTest
 @testable import ConcurrencyPlayground
 
 final class ConcurrencyPlaygroundTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    // 계좌 2개를 생성합니다.
+    let account1 = BankAccount(balance: 100)
+    let account2 = BankAccount(balance: 100)
+    
+    func testBankAccount() throws {
+        runAsyncTest { [weak self] in
+            guard let self else {
+                return
+            }
+            
+            print("테스트 시작")
+            await update()
+            print("계좌 입출금 진행")
+            await config()
+            print("테스트 완료")
+        }
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    private func transfer(amount: Int, from: BankAccount, to: BankAccount) async throws {
+        // 인출 후
+        let available = await from.withdraw(amount: amount)
+        
+        // 입금 시도
+        await to.deposit(amount: available)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    private func update() async {
+        // 비동기 작업 시작
+        Task {
+            try await transfer(amount: 75, from: account2, to: account1)
+            print("Transfer complete :: account2 => account1: 75 ")
+        }
+        
+        // 비동기 작업 시작
+        Task {
+            try await transfer(amount: 50, from: account1, to: account2)
+            print("Transfer complete :: account1 => account2 : 50 ")
         }
     }
 
+    private func config() async {
+        Task {
+            let balance1 = await account1.balanceConfig() // actor method
+            let balance2 = await account2.balanceConfig() // actor method
+            
+            XCTAssertTrue(balance1 == 125, "balance1: \(balance1), balance2: \(balance2)")
+            XCTAssert(balance2 == 75, "balance1: \(balance1), balance2: \(balance2)")
+        }
+    }
 }
